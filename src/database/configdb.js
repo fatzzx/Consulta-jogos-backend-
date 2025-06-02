fab59efcbd3f3d3689c6abc06e038c56ef3a7eb6
Gabriel Aragão <117946090+gabrielfrz@users.noreply.github.com>
2025-06-02 18:58:14 +0000

    Fixing mongodb

--- src/database/configdb.js
+++ src/database/configdb.js
@@ -1,15 +1,33 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 
-dotenv.config();
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("A variável de ambiente MONGO_URI não está definida.");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Conectado ao MongoDB');
+    cached.conn = await cached.promise;
+    return cached.conn;
   } catch (err) {
-    console.error('Erro na conexão com o MongoDB:', err);
-    process.exit(1);
+    cached.promise = null;
+    throw err;
   }
 };
 

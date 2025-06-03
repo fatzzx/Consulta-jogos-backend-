6b32441869cfa823fcdfd6f4ddc7a4fe71c32bde
Gabriel Aragão <117946090+gabrielfrz@users.noreply.github.com>
2025-06-03 02:36:36 +0000

    Fixing invalid date

--- src/models/Favorite.js
+++ src/models/Favorite.js
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose';
 
-const FavoriteSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  gameId: { type: String, required: true },
-  title: { type: String, required: true },
-  cover: { type: String },
-});
+const FavoriteSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    gameId: { type: String, required: true },
+    title: { type: String, required: true },
+    cover: { type: String },
+  },
+  {
+    timestamps: true, 
+);
 
 export default mongoose.model('Favorite', FavoriteSchema);

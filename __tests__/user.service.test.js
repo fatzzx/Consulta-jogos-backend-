8306aed5e025044d0de5d6a4c796f7d481978801
Gabriel Aragão <117946090+gabrielfrz@users.noreply.github.com>
2025-06-08 19:38:14 +0000

    feat: implementar testes e ajustes

--- __tests__/user.service.test.js
+++ __tests__/user.service.test.js
@@ -1,14 +1,22 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import * as userService from '../src/services/user.service.js';
 import User from '../src/models/User.js';
 
-// Mock da função User.create apenas
+/
 vi.mock('../src/models/User.js', () => ({
   default: {
     findOne: vi.fn(),
     create: vi.fn()
   }
 }));
+
+
+vi.mock('bcryptjs', () => ({
+  compare: vi.fn(),
+  hash: vi.fn()
+}));
+
+import * as userService from '../src/services/user.service.js';
+import * as bcrypt from 'bcryptjs';
 
 describe('User Service (teste simplificado)', () => {
   beforeEach(() => {
@@ -17,11 +25,13 @@ describe('User Service (teste simplificado)', () => {
 
   it('deve registrar um usuário novo (simplificado)', async () => {
     User.findOne.mockResolvedValue(null);
+    bcrypt.hash.mockResolvedValue('senhaHasheada');
+
     User.create.mockResolvedValue({
       _id: '1',
       name: 'João',
       email: 'joao@example.com',
-      password: 'senha123' // sem hash
+      password: 'senhaHasheada'
     });
 
     const result = await userService.registerUser({
@@ -33,5 +43,30 @@ describe('User Service (teste simplificado)', () => {
     expect(User.findOne).toHaveBeenCalledWith({ email: 'joao@example.com' });
     expect(User.create).toHaveBeenCalled();
     expect(result.name).toBe('João');
+  });
+
+  it('não deve autenticar o usuário com senha incorreta', async () => {
+    User.findOne.mockResolvedValue({
+      _id: '1',
+      name: 'João',
+      email: 'joao@example.com',
+      password: 'senhaHasheada'
+    });
+
+    bcrypt.compare.mockResolvedValue(false);
+
+    await expect(userService.loginUser({
+      email: 'joao@example.com',
+      password: 'senhaErrada'
+    })).rejects.toThrow('Senha incorreta');
+  });
+
+  it('deve lançar erro se o usuário não for encontrado', async () => {
+    User.findOne.mockResolvedValue(null);
+
+    await expect(userService.loginUser({
+      email: 'inexistente@example.com',
+      password: 'qualquerSenha'
+    })).rejects.toThrow('Usuário não encontrado');
   });
 });
